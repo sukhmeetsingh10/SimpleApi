1f72486da1b0d3d1f5a355f42ab4e5d8a735ef7a
sukhmeetsingh10 <sukhmeetsingh10@gmail.com>
2021-07-23 15:24:32 +0530

    first commit

---
+++ Test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        //Arrange
+         WeatherForecastController  wController = new WeatherForecastController();
+
+        [Fact]
+        public void Test1()
+        {
+            //Act
+            var output = wController.FetchData(1);
+            //assert
+            Assert.Equal("Rachna Singh",output);
+        }
+    }
+}
